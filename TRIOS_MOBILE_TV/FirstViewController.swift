12b334812265647a229cd9c67263f7419c2cd31b
stephen eshelman <objectthink@comcast.net>
2015-10-03 16:35:07 -0400

    Initial Commit

---
+++ TRIOS_MOBILE_TV/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TRIOS_MOBILE_TV
+//
+//  Created by stephen eshelman on 10/3/15.
+//  Copyright © 2015 objectthink.com. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
